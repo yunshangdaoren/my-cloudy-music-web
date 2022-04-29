ad0dacaffb4246ac97852520e4e1c1f59cf26b89
lqs <1396758907@qq.com>
2022-04-29 22:35:15 +0800

    add send-sms-code count down

--- js/sessions/new-sms.js
+++ js/sessions/new-sms.js
@@ -28,3 +28,74 @@ $(function() {
 		}
 	});
 });
+
+
+//找到短信发送验证码按钮
+let buttonSendSMSCode = $("#button-send-sms-code");
+
+/**
+ * 发送短信验证码按钮点击了
+ */
+function onSendSMSCodeClick(){
+	//获取手机号
+	let phone = $("#phone").val();
+	//判断手机号
+	if(!phone || phone==''){
+		alert("请输入手机号！");
+		return;
+	}
+	
+	//TODO 判断手机号格式
+	
+	//发送短信验证码
+	sendSMSCode(phone);
+}
+
+/**
+ * 发送短信验证码
+ */
+function sendSMSCode(data){
+	//TODO 发送验证码
+	
+	//开始倒计时
+	startCountDown();
+}
+
+/**
+ * 开始倒计时
+ */
+function startCountDown(){
+	//倒计时
+	var time = 5;
+	
+	//开启一个定时器
+	var timer = setInterval(function(){
+		if(time == 0){
+			//倒计时完成了
+			
+			//清楚定时器
+			clearInterval(timer);
+			
+			//启用短信发送验证码按钮
+			enableSendSMSCodeButton();
+		}else{
+			//正在倒计时
+			
+			//显示倒计时时间
+			buttonSendSMSCode.text(time+"秒");
+			//时间--
+			time--;
+		}
+	}, 1000);
+	
+	//禁用短信发送验证码按钮
+	buttonSendSMSCode.attr("disabled", true);
+}
+
+/**
+ * 启用短信发送验证码按钮
+ */
+function enableSendSMSCodeButton(){
+	buttonSendSMSCode.attr("disabled", false);
+	buttonSendSMSCode.text("发送验证码");
+}
